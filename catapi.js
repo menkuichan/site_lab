0c38dfedb80afcada4ea37a07800a28b95049e1a
iodkowski <idkwsk@gmail.com>
2019-03-28 17:46:17 +0300

    mongo & catapi added

---
+++ catapi.js
@@ -0,0 +1,8 @@
+const request = require('request');
+
+module.exports.getRandomCats = function (qt) {
+    request('https://api.thecatapi.com/v1/images/search?limit=' + qt, {json: true}, (err, res, body) => {
+        if (err) return console.log(err);
+        body.forEach(pic => console.log(pic.url));
+    });
+}
---
+++ mongo.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const DB_URL = "mongodb://localhost:27017/postsdb";
+
+// модель
+const postScheme = new Schema({
+        author_id: {
+            type: Number,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 30
+        },
+        text: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 1000
+        },
+        pics: {
+            type: [String],
+            required: true
+        },
+        date: {
+            type: Date,
+            required: true
+        }
+    },
+    {
+        versionKey: false
+    });
+
+mongoose.connect(DB_URL, {useNewUrlParser: true});
+const Post = mongoose.model("Post", postScheme);
+
+// создать пост
+module.exports.createPost = function (authorId, title, text, pics) {
+    Post.create({author_id: authorId, title: title, text: text, pics: pics, date: Date.now()}, function (err, post) {
+        mongoose.disconnect();
+        if (err) return console.log(err);
+        console.log("New post created:\n", post);
+    });
+}
+// получить все посты
+module.exports.getAllPosts = function () {
+    Post.find({}, function (err, posts) {
+        mongoose.disconnect();
+        if (err) return console.log(err);
+        console.log(posts);
+    });
+}
+// удалить все посты
+module.exports.removeAllPosts = function () {
+    Post.remove({}, function (err, result) {
+        mongoose.disconnect();
+        if (err) return console.log(err);
+        console.log(result);
+    });
+}

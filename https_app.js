d8349feab04ea7ebb26d13f3be1050311d8b7528
iodkowski <idkwsk@gmail.com>
2019-04-20 14:18:29 +0300

    fix

--- https_app.js
+++ https_app.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+import express from 'express';
+import fs from 'fs';
+import http from 'http';
+import https from 'https';
+
 const app = express();
 
 const privateKey = fs.readFileSync('https/localhost-privateKey.key', 'utf8');
@@ -11,10 +12,10 @@ const credentials = {key: privateKey, cert: certificate};
 // ...
 
 const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
 httpServer.listen(8080, () => {
     console.log("HTTP server is running on http://localhost:8080");
 });
+const httpsServer = https.createServer(credentials, app);
 httpsServer.listen(443, () => {
     console.log("HTTPS server is running on https://localhost:443");
 });

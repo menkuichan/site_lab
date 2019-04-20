f10f2b1bf2798d107fe6501633ccbd274ac57e7e
iodkowski <idkwsk@gmail.com>
2019-04-20 14:15:19 +0300

    servers fix

--- https_app.js
+++ https_app.js
@@ -12,7 +12,9 @@ const credentials = {key: privateKey, cert: certificate};
 
 const httpServer = http.createServer(app);
 const httpsServer = https.createServer(credentials, app);
-httpServer.listen(8080);
-console.log("HTTP server is running on http://localhost:8080");
-httpsServer.listen(443);
-console.log("HTTPS server is running on https://localhost:443");
+httpServer.listen(8080, () => {
+    console.log("HTTP server is running on http://localhost:8080");
+});
+httpsServer.listen(443, () => {
+    console.log("HTTPS server is running on https://localhost:443");
+});
